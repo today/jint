4e0d7ca2607978066a95f22b744d1eb8f3ec950c
Jint <today.xa@gmail.com>
2009-03-25 10:27:42 +0800

    hehe

--- jintlearn/learn.py
+++ jintlearn/learn.py
@@ -6,7 +6,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 class Recite(db.Model):
-  #uid = 
+  #uid =
   word = db.StringProperty(multiline=False)
   question = db.StringProperty(multiline=False)
   answer = db.StringProperty(multiline=False)
@@ -19,7 +19,7 @@ class MainPage(webapp.RequestHandler):
       nickname = user.nickname()
     else:
       self.redirect(users.create_login_url(self.request.uri))
-    
+
     self.response.out.write(getHtmlStart())
     self.response.out.write("""
       <style>
@@ -31,7 +31,7 @@ class MainPage(webapp.RequestHandler):
       <script>
       $(document).ready(function(){
         //alert("jquery");
-        
+
         $(".cls_right").hide();
         $(".cls_rightanswer").hide();
 
@@ -41,16 +41,16 @@ class MainPage(webapp.RequestHandler):
 
            var resultArea = $(this).next();
            var rightanswer = resultArea.children(".cls_rightanswer").html();
-           
+
            var key = resultArea.children("input")[0].value;
-           
+
 
            if( tryanswer == rightanswer ){
               resultArea.children(".cls_right").attr("src","images/right.png").show();
               resultArea.children(".cls_rightanswer").css("color","green");
 
               $(this).css("color","green");
-              
+
               go_result(1, key);
            }
            else{
@@ -58,7 +58,7 @@ class MainPage(webapp.RequestHandler):
 
               $(this).css("color","red");
               resultArea.children(".cls_rightanswer").css("color","red");
-              
+
               go_result(0, key);
            }
         });
@@ -73,39 +73,40 @@ class MainPage(webapp.RequestHandler):
             $(".cls_right", this).show();
           }
         );
-        
+
         $(".cls_purewin").each(function(i){
-           
+
            num = $(this).text();
            if( 10 < num ){
               $(this).parent().hide();
            }
-           
-        }); 
-        
+
+        });
+
         $("input[name='b1']").click(function(event){
           $(".cls_exam").show();
-        }); 
+        });
       });
-      
+
       var go_result = function(right, key){
         rightFlag = '&wrong=1';
         if( 1 == right ){
           rightFlag = '&right=1';
         }
         
-        url = '/record?w=achieve' + rightFlag + '&key=' + key;
-        
-        //alert(url);
+        word = $(".cls_word").text();       
+
+        url = '/record?w='+ word + rightFlag + '&key=' + key;
+
         $.ajaxSetup({
           cache: false,
           type: "GET"
         });
         $.get(url,{}, function(data){
           //alert("Data Loaded: " + data);
-        }); 
-        
-        
+        });
+
+
       }
       </script>
       """)
@@ -113,38 +114,38 @@ class MainPage(webapp.RequestHandler):
     req_word = 'achieve'
     if self.request.get('w'):
       req_word = self.request.get('w')
-      
-    self.response.out.write('<div><span class="cls_big">Hello, %s! The word is <b>%s</b> </span>' % (nickname, req_word) )
+
+    self.response.out.write('<div><span class="cls_big">Hello, %s! The word is <span class="cls_word">%s</span> </span>' % (nickname, req_word) )
     self.response.out.write('<span class="cls_rightalign"><input type="button" name="b1" value="Show All"></span></div><hr/>' )
 
     results222 = db.GqlQuery("SELECT * FROM Result WHERE word=:1 AND owner=:2", req_word, user )
     resultDict = {}
     for result222 in results222:
         resultDict[result222.reciteKey.key()] = result222
-    
+
     recites = db.GqlQuery("SELECT * FROM Recite WHERE word=:1 LIMIT 30",  req_word )
-    
+
 
     for recite in recites:
-      
+
       #results = db.GqlQuery("SELECT * FROM Result WHERE reciteKey=:1 ",  recite.key() )
-      
+
       rightCount = 0
       wrongCount = 0
       if resultDict.has_key(recite.key()):
-        result = resultDict[recite.key()]      
+        result = resultDict[recite.key()]
         rightCount = result.rightCount
         wrongCount = result.wrongCount
-      
-      
-      
-      
+
+
+
+
       #for result in results:
       #  rightCount = result.rightCount
       #  wrongCount = result.wrongCount
 
       self.response.out.write('<div class="cls_exam">')
-      self.response.out.write('<span>%s &nbsp;&nbsp; %s-%s=</span><span class="cls_purewin" ><b>%s</b></span>' % (recite.question, rightCount, wrongCount, rightCount-wrongCount ) )
+      self.response.out.write('<span>%s &nbsp;&nbsp; %s-%s = </span><span class="cls_purewin" ><b>%s</b></span>' % (recite.question, rightCount, wrongCount, rightCount-wrongCount ) )
       self.response.out.write('<br/><input type="text" name="tryanswer" value="" size="80" class="cls_try">' )
       self.response.out.write('<span class="cls_result"><br/><span class="cls_rightanswer">%s</span>' % recite.answer)
       self.response.out.write('<img src="images/right.png" class="cls_right" />')
@@ -167,11 +168,11 @@ def getHtmlStart():
   return """<html>
     <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <title>jint.org</title>
+    <title>http://jintlearn.appspot.com/</title>
     <!--    -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js"></script>
-    
-    <!--    
+
+    <!--
     <script type="text/javascript" src="images/jquery.js"></script>
     -->
     </head>
@@ -210,23 +211,23 @@ class recordResult(webapp.RequestHandler):
       nickname = user.nickname()
     else:
       self.redirect(users.create_login_url(self.request.uri))
-    
+
     textKey = self.request.get("key")
     if textKey:
       objKey = db.Key(textKey)
     else:
       return ""
-    
+
     word = self.request.get('w')
-      
+
     right = 0
     if self.request.get("right"):
       right = 1
-      
+
     wrong = 0
     if self.request.get("wrong"):
       wrong = 1
-    
+
     #result = db.get(objKey)  #Result(reciteKey=objKey, owner=user)
     results = db.GqlQuery("SELECT * FROM Result WHERE owner = :1 and reciteKey= :2 and word= :3", user, objKey, word )
     result = results.get()
@@ -242,7 +243,7 @@ class recordResult(webapp.RequestHandler):
       result.wrongCount = 0
       result.rightCount = result.rightCount + right
       result.wrongCount = result.wrongCount + wrong
-    
+
     result.put()
     #self.redirect('/?w=' + result.word )
 
